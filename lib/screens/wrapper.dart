69892d74ee3d8ea5563bae72f4ed4388511e5be2
Emmanuel Egomson <emmanuel@Emmanuels-MacBook-Pro.local>
2020-08-22 10:29:47 -0500

    implement stream provider package for app user

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,16 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import "package:flutter/material.dart";
 import 'package:brew_crew/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+//    access the user data from the provider
+  final user = Provider.of<AppUser>(context);
+  print(user);
 //  Return either home or authenticate widget
     return Authenticate();
   }
