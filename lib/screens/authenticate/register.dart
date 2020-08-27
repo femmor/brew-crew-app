7607802677f5c077a03acff5ae883e6066c590ae
Emmanuel Egomson <fegomson@gmail.com>
2020-08-26 19:00:15 -0500

    add form key to the Global formState

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,18 +2,17 @@ import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
-
   final Function toggleView;
-  Register ({ this.toggleView});
+  Register({this.toggleView});
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
-
   // Create an instance of the AuthService function
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
 //  Text field state
   String email = "";
@@ -26,58 +25,72 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
-        title: Text("Sign up to Brew Crew", style: TextStyle(
-          fontSize: 26.0,
-          fontWeight: FontWeight.bold,
-        ),),
+        title: Text(
+          "Sign up to Brew Crew",
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         actions: [
           FlatButton.icon(
-            onPressed: (){
+            onPressed: () {
               widget.toggleView();
             },
-            icon: Icon(Icons.person, color: Colors.grey[100],),
-            label: Text("Sign In", style: TextStyle(
+            icon: Icon(
+              Icons.person,
               color: Colors.grey[100],
-              fontSize: 16.0,
-            )),
+            ),
+            label: Text("Sign In",
+                style: TextStyle(
+                  color: Colors.grey[100],
+                  fontSize: 16.0,
+                )),
           )
         ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               TextFormField(
-                decoration: InputDecoration(
-                    hintText: "email address"
-                ),
-                onChanged: (val){
+                decoration: InputDecoration(hintText: "email address"),
+                onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               TextFormField(
-                decoration: InputDecoration(
-                    hintText: "password"
-                ),
+                decoration: InputDecoration(hintText: "password"),
                 obscureText: true,
-                onChanged: (val){
+                onChanged: (val) {
                   setState(() => password = val);
                 },
               ),
-              SizedBox(height: 20.0,),
+              SizedBox(
+                height: 20.0,
+              ),
               RaisedButton(
                 onPressed: () async {
-                  print("Email: "+email +" Password: "+password);
+                  if (_formKey.currentState.validate()) {
+                    print(email + " " + password);
+                  }
                 },
                 color: Colors.brown[800],
-                child: Text("Register", style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-
-                ),),
+                child: Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
               ),
             ],
           ),
