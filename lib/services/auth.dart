c177c2a28765a56722c089d02c00fc74f7c36ebc
Emmanuel Egomson <fegomson@gmail.com>
2020-09-10 18:55:10 -0500

    create firestore user record

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/user.dart';
+import 'package:brew_crew/services/database.dart';
 import "package:firebase_auth/firebase_auth.dart";
 
 class AuthService {
@@ -50,11 +51,18 @@ class AuthService {
 //  register with email and password
   Future registerWithEmailAndPassword(String email, String password) async {
     try {
+      // create a new user with email and password
       UserCredential result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
-      //Firebase user
+      //Firebase user (Get the user back - with a uid property created by firebase)
       User user = result.user;
-      //Turn firebase user into user
+
+      // create a new document for the user with uid created by firebase
+      // create an instance of the database service
+      // pass in the uid from that user that comes back
+      // call updateUserDate and pass in the userData for the new user
+      await DatabaseService(uid: user.uid).updateUserData("Emmanuel", "0", 100);
+      // Turn firebase user into user
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,21 @@
 import "package:cloud_firestore/cloud_firestore.dart";
 
 class DatabaseService {
+  final String uid;
+  DatabaseService({this.uid});
+
 //  collection reference
   final CollectionReference brewCollection =
       FirebaseFirestore.instance.collection("brews");
+  Future updateUserData(
+    String name,
+    String sugars,
+    int strength,
+  ) async {
+    return await brewCollection.doc(uid).set({
+      "name": name,
+      "sugars": sugars,
+      "strength": strength,
+    });
+  }
 }
