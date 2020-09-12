a2e347702ed1fa22628b5d473e2589db5eebb9cd
Emmanuel Egomson <fegomson@gmail.com>
2020-09-11 19:46:30 -0500

    fetching list brew data

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,22 +12,19 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>>(context);
-    if (brews != null) {
-      brews.forEach((brew) {
-        print(brew.name);
-        print(brew.sugars);
-        print(brew.strength);
-      });
-    }
+
+    return ListView.builder(
+      itemCount: brews.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
+
     // print(brews.docs);
     // if (brews != null) {
     //   for (var doc in brews.docs) {
     //     print(doc.data());
     //   }
     // }
-
-    return Scaffold(
-      body: Text("Brew list"),
-    );
   }
 }
