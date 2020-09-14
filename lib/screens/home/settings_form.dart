3a5ab4a793d4a1b1d3b861382ddcf0c6e93c92f8
Emmanuel Egomson <fegomson@gmail.com>
2020-09-14 13:22:14 -0400

    updating user data

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -97,9 +97,14 @@ class _SettingsFormState extends State<SettingsForm> {
                   ),
                   RaisedButton(
                     onPressed: () async {
-                      print(_currentName);
-                      print(_currentSugars);
-                      print(_currentStrength);
+                      if (_formKey.currentState.validate()) {
+                        await DatabaseService(uid: user.uid).updateUserData(
+                          _currentName ?? userData.name,
+                          _currentSugars ?? userData.sugars,
+                          _currentStrength ?? userData.strength,
+                        );
+                        Navigator.pop(context);
+                      }
                     },
                     color: Colors.pink,
                     child: Text(
